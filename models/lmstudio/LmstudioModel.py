ea5d0ece654eaa0acb50687ef189c9e4ce953969
Marco <marco23.riva@mail.polimi.it>
2026-02-19 18:37:28 +0100

    improved lmstudio for remote exeuction

--- models/lmstudio/LmstudioModel.py
+++ models/lmstudio/LmstudioModel.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass, field
+
 from lmstudio import LMStudioError
 
 from models.model import Model
@@ -6,18 +8,24 @@ from queries.test import Query
 
 class LmstudioModel(Model):
 
-    def _submit_direct_inline(self, prompt: str) -> str:
+    address: str = "localhost:1234"
+    @dataclass
+    class Params(Model.Params):
+        retries: int = 3
+    params: Params = field(default_factory=Params)
+
+    def _submit_direct_inline(self, prompt: str, format) -> str:
         import lmstudio as lms
-        server_api_host = "localhost:1234"
-        retries = 3
+        retries = self.params.retries
 
         while retries:
             try:
-                with lms.Client(server_api_host) as client:
+                with lms.Client(self.address) as client:
                     model = client.llm.model(self.name_api)
 
                     response = model.respond(
                         prompt,
+                        response_format=format,
                         on_prompt_processing_progress = (lambda progress: print(f"{progress*100}% complete")))
 
                 return response.content
@@ -39,7 +47,7 @@ class LmstudioModel(Model):
         cache = CacheFactory.create_cache(cache_config)
 
         lm = LM(model="lm_studio/" + self.name_api,
-                api_base="http://127.0.0.1:1234/v1",
+                api_base=f"http://{self.address}/v1",
                 max_tokens=self.max_tokens if self.max_tokens else None,
                 cache=cache,
                 temperature=0.6)
@@ -50,7 +58,7 @@ class LmstudioModel(Model):
 
     def _submit_direct(self, queries: list[Query]) -> None:
         for query in queries:
-            query.response = self._submit_direct_inline(query.prompt)
+            query.response = self._submit_direct_inline(query.prompt, query.response_json_schema)
 
     def _submit_lotus(self, queries: list[Query], df) -> None:
         for query in queries:
